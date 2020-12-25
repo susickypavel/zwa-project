fa5737e941d1a50d5d85c0be3803337ff6b88042
Pavel Sušický <pavels.sus@gmail.com>
2020-12-25 17:57:14 +0100

    feat: user, user fixtures, db migration

---
+++ migrations/Version20201225155307.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201225155307 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX uniq_8d93d649d17f50a6');
+        $this->addSql('ALTER TABLE "user" RENAME COLUMN uuid TO email');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74');
+        $this->addSql('ALTER TABLE "user" RENAME COLUMN email TO uuid');
+        $this->addSql('CREATE UNIQUE INDEX uniq_8d93d649d17f50a6 ON "user" (uuid)');
+    }
+}
