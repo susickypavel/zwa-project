fa5affca357cc9680f58feb4e9abb993c407c5d4
Pavel Sušický <pavels.sus@gmail.com>
2020-12-27 19:48:17 +0100

    feat: redirect user from /login page if logged in

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,9 +14,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-//         if ($this->getUser()) {
-//             return $this->redirectToRoute('/random');
-//         }
+         if ($this->getUser()) {
+             return $this->redirectToRoute('root');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
