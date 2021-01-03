a05fe52c0f5911967670da2952fc6c12863d89d3
Pavel Sušický <pavels.sus@gmail.com>
2021-01-03 16:16:15 +0100

    feat: world upload form

---
+++ src/Form/WorldUploadType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\WorldUpload;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class WorldUploadType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("saveGameInfo", FileType::class, [
+                "mapped" => false,
+                "constraints" => [
+                    new File([
+                        "maxSize" => "5M"
+                    ]),
+                    new NotNull([
+                        "message" => "Prosím nahrajte soubor SaveGameInfo"
+                    ])
+                ]
+            ])
+            ->add("worldState", FileType::class, [
+                "mapped" => false,
+                "constraints" => [
+                    new File([
+                        "maxSize" => "5M"
+                    ]),
+                    new NotNull([
+                        "message" => "Prosím nahrajte soubor"
+                    ])
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => WorldUpload::class,
+        ]);
+    }
+}
