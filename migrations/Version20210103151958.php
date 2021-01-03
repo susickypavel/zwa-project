b43ebf1494b0becf6bd7cb0a4e182374188f442d
Pavel Sušický <pavels.sus@gmail.com>
2021-01-03 16:24:10 +0100

    feat: world upload paths to files

---
+++ migrations/Version20210103151958.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210103151958 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE world_upload ADD save_game_info VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE world_upload ADD world_state VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE world_upload DROP save_game_info');
+        $this->addSql('ALTER TABLE world_upload DROP world_state');
+    }
+}
