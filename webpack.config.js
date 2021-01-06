3da8266517d565ef220b996259d9edfaf5e2f173
Pavel Sušický <pavels.sus@gmail.com>
2021-01-06 15:46:17 +0100

    feat: symfony encore

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+
+    .addStyleEntry("tailwind", "./assets/styles/tailwind.css")
+    .enablePostCssLoader((options) => {
+        options.config = {
+            path: "./postcss.config.js"
+        }
+    })
+
+    .splitEntryChunks()
+
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    });
+
+module.exports = Encore.getWebpackConfig();
