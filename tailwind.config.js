78ce4c93afc034b749d76d724433e374a76551cd
Pavel Sušický <pavels.sus@gmail.com>
2021-01-06 20:06:36 +0100

    feat: tailwindcss dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const darkmode = require('tailwindcss-dark-mode');
+
 module.exports = {
   theme: {
     extend: {
@@ -6,4 +8,12 @@ module.exports = {
       }
     }
   },
+  variants: {
+    backgroundColor: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd'],
+    borderColor: ['dark', 'dark-disabled', 'dark-focus', 'dark-focus-within'],
+    textColor: ['dark', 'dark-hover', 'dark-active', 'dark-placeholder']
+  },
+  plugins: [
+      darkmode()
+  ]
 }
